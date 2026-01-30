fb70b89e1f7ecbcef2294114c8145e1bca6a60b4
kittivong <kittivong@gmail.com>
2026-01-30 12:03:12 +0700

    upgrade to spring boot 4.0.2

--- src/main/java/th/go/dxc/app/util/RtpNationalCriminalOffenceEntityToModelConverter.java
+++ src/main/java/th/go/dxc/app/util/RtpNationalCriminalOffenceEntityToModelConverter.java
@@ -5,7 +5,7 @@ import org.springframework.core.convert.converter.Converter;
 
 import th.go.dxc.app.model.RtpNationalCriminalOffence;
 import th.go.dxc.infra.datasource.policeraw.entity.RtpNationalCriminalOffenceEntity;
-
+ 
 @Mapper
 public interface RtpNationalCriminalOffenceEntityToModelConverter extends Converter<RtpNationalCriminalOffenceEntity, RtpNationalCriminalOffence>{
 	public RtpNationalCriminalOffence convert(RtpNationalCriminalOffenceEntity source);
--- src/main/java/th/go/dxc/app/util/RtpNationalCriminalOffenceFilterToEntityFilterConverter.java
+++ src/main/java/th/go/dxc/app/util/RtpNationalCriminalOffenceFilterToEntityFilterConverter.java
@@ -5,7 +5,7 @@ import org.springframework.core.convert.converter.Converter;
 
 import th.go.dxc.app.model.RtpNationalCriminalOffenceFilter;
 import th.go.dxc.infra.datasource.policeraw.entity.RtpNationalCriminalOffenceEntityFilter;
-
+ 
 @Mapper
 public interface RtpNationalCriminalOffenceFilterToEntityFilterConverter extends Converter<RtpNationalCriminalOffenceFilter, RtpNationalCriminalOffenceEntityFilter> {
 	public RtpNationalCriminalOffenceEntityFilter convert(RtpNationalCriminalOffenceFilter source);
